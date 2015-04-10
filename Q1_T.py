9121fb3cc4f36942a86c38736b373e0d4b65f92a
Vlachopanagiotis Theocharis <tvlahopanagiotis@gmail.com>
2015-04-11 01:17:13 +0300

    Question 1

--- Q1_T.py
+++ Q1_T.py
@@ -5,10 +5,10 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 """
 sum=0
 i=0
-for i in range(1,1000)
-	if i % 3 = 0
+for i in range(1,1000):
+	if i % 3 == 0:
 		sum = sum + i
-	if i % 5 = 0
+	if i % 5 == 0:
 		sum = sum + i
 
 print sum
