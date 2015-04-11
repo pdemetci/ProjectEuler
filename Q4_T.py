8bbb140c5ededb5d2c7d655792aeacc06aabcf16
Vlachopanagiotis Theocharis <tvlahopanagiotis@gmail.com>
2015-04-11 19:37:24 +0300

    Question 4 (correct) :heart_eyes:

--- Q4_T.py
+++ Q4_T.py
@@ -3,16 +3,17 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 '''
-
+largest_palindrome = 0
 
 for i in range (100,999):
 	for j in range (100,999):
 		number = i * j
 		number_string = str(number)
-		if number_string[0] == number_string[-1] and number_string[1] == number_string[-2]:
+		if number_string[0] == number_string[-1] and number_string[1] == number_string[-2]:				#instead of one big line
 			if number_string[2] == number_string[-3] and number_string[3] == number_string[-4]:
-				largest_palindrome = number
-				first_factor = i
-				second_factor = j
+				if number > largest_palindrome:
+					largest_palindrome = number
+					first_factor = i
+					second_factor = j
 
 print largest_palindrome , first_factor , second_factor , ",woohoo! :D"
