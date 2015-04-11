b2a46106f25920e91092843ffa61f5679cc75b5e
Vlachopanagiotis Theocharis <tvlahopanagiotis@gmail.com>
2015-04-11 19:32:31 +0300

    Question 4 (revised, still wrong)

--- Q4_T.py
+++ Q4_T.py
@@ -9,7 +9,10 @@ for i in range (100,999):
 	for j in range (100,999):
 		number = i * j
 		number_string = str(number)
-		if number_string[0] == number_string[-1] and number_string[1] == number_string[-2] and number_string[2] == number_string[-3]:
-			largest_palindrome = number
+		if number_string[0] == number_string[-1] and number_string[1] == number_string[-2]:
+			if number_string[2] == number_string[-3] and number_string[3] == number_string[-4]:
+				largest_palindrome = number
+				first_factor = i
+				second_factor = j
 
-print largest_palindrome , ",woohoo! :D"
+print largest_palindrome , first_factor , second_factor , ",woohoo! :D"
