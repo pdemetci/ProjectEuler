5b8c45eb22aebd48fb650ad21373ce7995502ab9
Pinar Demetci <pinardemetci@gmail.com>
2015-05-22 15:23:09 -0400

    Update to Q5 by Pinar

--- Q3_T.py
+++ Q3_T.py
@@ -5,11 +5,7 @@ What is the largest prime factor of the number 600851475143 ?
 """
 list_of_prime_factors = []
 max_prime_factor = 1
-<<<<<<< HEAD
 number = 24
-=======
-number = 125
->>>>>>> origin/master
 sum_list=1
 product = 1
 
@@ -19,24 +15,17 @@ for i in range (1,1000000):
 		max_prime_factor = i
 		list_of_prime_factors.append(i)
 
-<<<<<<< HEAD
-print list_of_prime_factors
+print list_of_prime_factors	
 
 for i in range (0,len(list_of_prime_factors)-1):
-		if list_of_prime_factors[i] % list_of_prime_factors[-i] == 0:
-			list_of_prime_factors.remove(list_of_prime_factors[i-1])
-			max_prime_factor = list_of_prime_factors[-1]
+	if list_of_prime_factors[i] % list_of_prime_factors[-i] == 0:
+		list_of_prime_factors.remove(list_of_prime_factors[i-1])
+		max_prime_factor = list_of_prime_factors[-1]
 
 print list_of_prime_factors
-
 
 for i in range(0,len(list_of_prime_factors)):
 	product = product * list_of_prime_factors[i]
 	if product == number:
-=======
-for i in range(0,len(list_of_prime_factors)):
-	product = product * list_of_prime_factors[i]
-	if product == 125:
->>>>>>> origin/master
 		print max_prime_factor
 		print "Woohoo!!! :D "
